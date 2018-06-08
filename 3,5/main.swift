89315cfeb8926d593d4d2d780567e8388d04d292
Gregory Newman-Smith <GregoryNewman-Smith@gregorys-mbp.home>
2018-06-08 18:40:01 +0100

    Initial Commit

---
+++ 3,5/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  3,5
+//
+//  Created by Gregory Newman-Smith on 08/06/2018.
+//  Copyright © 2018 Gregory Newman-Smith. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
